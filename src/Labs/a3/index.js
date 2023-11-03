d6405fce4f11f434aeb81a02b7fd2afd6b97725f
Evan Ritzcovan <evanritzcovan@gmail.com>
2023-11-03 14:40:54 -0400

    lab 4

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -4,19 +4,27 @@ import DynamicStyling from "./DynamicStyling";
 import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
 import Styles from "./Styles";
-import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+    const { todos } = useSelector((state) => state.todosReducer);
     return (
         <div className="container">
             <h1>Assignment 3</h1>
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
+
             <JavaScript/>
             <PathParameters/>
             <DynamicStyling/>
             <Classes/>
             <Styles/>
             <ConditionalOutput/>
-            <TodoList/>
         </div>
     );
 }
---
+++ src/Labs/a4/ReduxExamples/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import HelloReducer from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
+
+const ReduxExamples = () => {
+  return (
+    <div>
+      <h2>Redux Examples</h2>
+
+     
+
+      
+      <AddRedux />
+      <hr />
+      <CounterRedux />
+      <hr />
+      <HelloReducer />
+      <hr />
+    </div>
+  );
+};
+
+export default ReduxExamples;
